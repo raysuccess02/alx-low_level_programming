3d229da3e230ab4c09eb904a21a56c30e684684b
raysuccess02 <rayofsucces@yahoo.com>
2022-12-19 03:58:45 -0800

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - update pointer value to 98
+ * @n:parameters to be provided
+ * Returns: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
