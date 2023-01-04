16f00a64d1e3733e2471b57bfc401cc0b6c9bca1
raysuccess02 <rayofsucces@yahoo.com>
2023-01-04 04:56:05 -0800

    Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -21,3 +21,25 @@ int evaluate_num(int num, int iterator)
 	}
 	return (0);
 }
+/**
+ * is_prime_number - evaluate prime or not
+ * @num: number
+ * Return: return 1 prime - return 0 otherwise
+ */
+int is_prime_number(int num)
+{
+	int iterator;
+
+	iterator = 2;
+
+	/* only greater than 2*/
+	if (num < 2)
+	{
+		return (0);
+	}
+	if (num == 2)
+	{
+		return (1);
+	}
+	return (evaluate_num(num, iterator));
+}
