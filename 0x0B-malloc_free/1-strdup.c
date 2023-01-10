ea2d0e612cc19373e35da900dc1bb922e5f65aca
raysuccess02 <rayofsucces@yahoo.com>
2023-01-10 03:01:33 -0800

    Malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strlen - count array
+ * @s: array of elements
+ * Return: 1
+ */
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != '\0') /*count character of string*/
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * _strcpy - copy arrays
+ * @src: array of elements
+ * @dest: dest array
+ * Return: dest
+ */
